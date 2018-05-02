7b1dbce87b5fa287fc1d90ac69d566dda813017f
samuel sanchez <shamelo9510@gmail.com>
2018-05-02 09:57:11 -0500

    design app

---
+++ app/src/main/java/sanchez/samuel/coagronorte/develop/servidores/MainActivity.java
@@ -0,0 +1,27 @@
+package sanchez.samuel.coagronorte.develop.servidores;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.newServer)
+    public void abrir(){
+        Intent intent = new Intent(getApplicationContext(),RegistreServer.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/sanchez/samuel/coagronorte/develop/servidores/RegistreServer.java
@@ -0,0 +1,14 @@
+package sanchez.samuel.coagronorte.develop.servidores;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistreServer extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registre_server);
+    }
+}
